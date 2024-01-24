ef652b9d541ad1a05d9af000a6b1bd918ec297fc
Audrow Nash <audrow@intrinsic.ai>
2024-01-24 19:39:55 +0000

    Rebase from 'debian/rolling/cyclonedds'

--- src/core/ddsc/src/dds_writer.c
+++ src/core/ddsc/src/dds_writer.c
@@ -167,6 +167,25 @@ static uint32_t get_bandwidth_limit (dds_transport_priority_qospolicy_t transpor
 #endif
 }
 
+void dds_writer_invoke_cbs_for_pending_events(struct dds_entity *e, uint32_t status)
+{
+  dds_writer * const wr = (dds_writer *) e;
+  struct dds_listener const * const lst =  &e->m_listener;
+
+  if (lst->on_publication_matched && (status & DDS_PUBLICATION_MATCHED_STATUS)) {
+    status_cb_publication_matched_invoke(wr);
+  }
+  if (lst->on_liveliness_lost && (status & DDS_LIVELINESS_LOST_STATUS)) {
+    status_cb_liveliness_lost_invoke(wr);
+  }
+  if (lst->on_offered_incompatible_qos && (status & DDS_OFFERED_INCOMPATIBLE_QOS_STATUS)) {
+    status_cb_offered_incompatible_qos_invoke(wr);
+  }
+  if (lst->on_offered_deadline_missed && (status & DDS_OFFERED_DEADLINE_MISSED_STATUS)) {
+    status_cb_offered_deadline_missed_invoke(wr);
+  }
+}
+
 static void dds_writer_interrupt (dds_entity *e) ddsrt_nonnull_all;
 
 static void dds_writer_interrupt (dds_entity *e)
@@ -280,7 +299,8 @@ const struct dds_entity_deriver dds_entity_deriver_writer = {
   .set_qos = dds_writer_qos_set,
   .validate_status = dds_writer_status_validate,
   .create_statistics = dds_writer_create_statistics,
-  .refresh_statistics = dds_writer_refresh_statistics
+  .refresh_statistics = dds_writer_refresh_statistics,
+  .invoke_cbs_for_pending_events = dds_writer_invoke_cbs_for_pending_events
 };
 
 #ifdef DDS_HAS_SHM
@@ -452,6 +472,12 @@ dds_entity_t dds_create_writer (dds_entity_t participant_or_publisher, dds_entit
   wr->m_whc = whc_new (gv, wrinfo);
   whc_free_wrinfo (wrinfo);
   wr->whc_batch = gv->config.whc_batch;
+  // We now have the QoS which defaults to "false", but it used to be controlled by a global setting
+  // (that most people were sensible enough to leave at false and that this deprecated now).  Or'ing
+  // the two together is perhaps a bit simplistic because it doesn't allow you to enable it globally
+  // and then disable it for a specific writer.  Should somebody runs into a problem because of this
+  // we can have another look.
+  wr->whc_batch = wqos->writer_batching.batch_updates || gv->config.whc_batch;
 
 #ifdef DDS_HAS_SHM
   assert(wqos->present & QP_LOCATOR_MASK);
